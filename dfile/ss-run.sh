239c9661d1d299ee57dd9c62d3cc60c7e92fea51
alicewww <alicewww@users.noreply.github.com>
2015-09-10 10:24:48 +0000

    restart shadowsocks change to one day once

--- dfile/ss-run.sh
+++ dfile/ss-run.sh
@@ -10,8 +10,15 @@
 	while true
 	do
 		/usr/bin/ss-server -s 0.0.0.0 -p 20000 -k ${PASSWD} -m aes-128-cfb -u --fast-open &
-		sleep 60
-		jobs -l | grep ss-server | awk '{print $2}' | xargs kill
+		/usr/bin/ss-server -s 0.0.0.0 -p 20000 -k ${PASSWD} -m aes-128-cfb -u --fast-open &
+		while true
+		do 
+			if [ "$(date +%H%M)" == "0300" ]; then
+				break
+			fi
+			sleep 60
+		done
+		jobs -l | grep ss-server | awk '{print $2}' | xargs kill -9
 		fg
 	done
 } >>/ss/ss.log 2>&1
